dfd3c9e30031f8cafa2f2e48aa84e2d6aa29358a
arish11 <abhishekarya.8596@gmail.com>
2023-06-22 13:57:07 +0530

    asssignment-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles : [`
+  .makeItWhite{
+    color:white;
+  }
+  `]
 })
 export class AppComponent {
   title = 'assignment_3';
+
+  buttonClicked : boolean = false
+  clicks: number[] = [];
+  counter = 0
+
+  onClick(){
+    this.buttonClicked === true ? this.buttonClicked = false : this.buttonClicked = true
+    this.counter++
+    this.clicks.push(this.counter)
+  }
+  
 }
